a92de274074ff934a92907b3e5c436ef78001bd2
Ashlen <dev@anthes.is>
2023-09-12 11:13:30 -0600

    test(test_validate_network_port): separate functions
    Dedicated functions for valid and invalid ports.

--- tests/test_validate_network_port.py
+++ tests/test_validate_network_port.py
@@ -4,11 +4,13 @@ from wg_to_hostname_if import validate_network_port
 import pytest
 
 
-def test_validate_network_port():
+def test_valid_ports():
     assert validate_network_port(80) == 80
     assert validate_network_port(0) == 0
     assert validate_network_port(65535) == 65535
 
+
+def test_invalid_ports():
     with pytest.raises(ValueError):
         validate_network_port(-1)
 
