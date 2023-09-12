563b2b5a0e7083a12f4b25a3d1ae196563262478
Ashlen <dev@anthes.is>
2023-09-12 11:12:33 -0600

    test(test_to_str): remove unneeded functions

--- tests/test_to_str.py
+++ tests/test_to_str.py
@@ -5,15 +5,9 @@ from wg_to_hostname_if import to_str
 
 def test_bytes_to_str():
     assert to_str(b"hello") == "hello"
+    assert to_str(b"") == ""
 
 
 def test_str_to_str():
     assert to_str("world") == "world"
-
-
-def test_empty_bytes_to_str():
-    assert to_str(b"") == ""
-
-
-def test_empty_str_to_str():
     assert to_str("") == ""
