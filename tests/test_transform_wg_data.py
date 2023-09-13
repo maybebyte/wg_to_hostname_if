cbc11cfa5cd226624ea92e2a022587d88840198e
Ashlen <dev@anthes.is>
2023-09-13 12:37:08 -0600

    test(transform_wg_data): fix broken tests, simplify
    These tests broke with commit c5cdb4933340f767ce905efbf4badf1d4d44d236
    because the change created a KeyError exception that wasn't expected.
    Rearrange them to fix this and use a function to create basic data
    while there.

--- tests/test_transform_wg_data.py
+++ tests/test_transform_wg_data.py
@@ -5,57 +5,44 @@ import pytest
 import ipaddress
 
 
-def test_basic_config():
-    wg_config_data = {
-        "endpoint": "192.168.1.1:51820",
-        "allowed_ips": "10.0.0.0/24,192.168.0.0/16",
-        "address": "10.0.0.1/32,192.168.1.1/32",
-    }
-    expected_result = {
-        "endpoint": ["192.168.1.1", "51820"],
+def create_basic_data():
+    return {
+        "address": "192.168.1.23/32,fd12:3456:789a:1::1/128",
+        "allowed_ips": "0.0.0.0/0,::0/0",
+        "endpoint": "73.60.105.217:11507",
+        "private_key": "yPlLc8Frd05JcfMBzs/p+53b5tfX29WhQbwuyJkdnEs=",
+        "public_key": "YV1sIDB+wgwYqlgxAGnvo2h80v+r7Y5BgHKLoYwt2Xg=",
+    }, {
+        "endpoint": ["73.60.105.217", "11507"],
         "allowed_ips": [
-            ipaddress.ip_interface("10.0.0.0/24"),
-            ipaddress.ip_interface("192.168.0.0/16"),
+            ipaddress.ip_interface("0.0.0.0/0"),
+            ipaddress.ip_interface("::0/0"),
         ],
         "address": [
-            ipaddress.ip_interface("10.0.0.1"),
-            ipaddress.ip_interface("192.168.1.1"),
+            ipaddress.ip_interface("192.168.1.23"),
+            ipaddress.ip_interface("fd12:3456:789a:1::1"),
         ],
+        "private_key": "yPlLc8Frd05JcfMBzs/p+53b5tfX29WhQbwuyJkdnEs=",
+        "public_key": "YV1sIDB+wgwYqlgxAGnvo2h80v+r7Y5BgHKLoYwt2Xg=",
     }
+
+
+def test_basic_config():
+    wg_config_data, expected_result = create_basic_data()
     assert transform_wg_data(wg_config_data) == expected_result
 
 
 def test_config_with_spaces():
-    wg_config_data = {
-        "endpoint": "192.168.1.1:51820",
-        "allowed_ips": "10.0.0.0/24, 192.168.0.0/16",
-        "address": "10.0.0.1/32, 192.168.1.1/32",
-    }
-    expected_result = {
-        "endpoint": ["192.168.1.1", "51820"],
-        "allowed_ips": [
-            ipaddress.ip_interface("10.0.0.0/24"),
-            ipaddress.ip_interface("192.168.0.0/16"),
-        ],
-        "address": [
-            ipaddress.ip_interface("10.0.0.1"),
-            ipaddress.ip_interface("192.168.1.1"),
-        ],
-    }
+    wg_config_data, expected_result = create_basic_data()
+    wg_config_data["address"] = "192.168.1.23/32, fd12:3456:789a:1::1/128"
+    wg_config_data["allowed_ips"] = "0.0.0.0/0, ::0/0"
     assert transform_wg_data(wg_config_data) == expected_result
 
 
 def test_config_with_invalid_ips():
-    wg_config_data = {
-        "endpoint": "192.168.1.1:51820",
-        "allowed_ips": "10.0.0.0/24,invalid_ip",
-        "address": "bogus,192.168.1.1/32",
-    }
-    expected_result = {
-        "endpoint": ["192.168.1.1", "51820"],
-        "allowed_ips": [ipaddress.ip_interface("10.0.0.0/24")],
-        "address": [ipaddress.ip_interface("192.168.1.1/32")],
-    }
+    wg_config_data, expected_result = create_basic_data()
+    wg_config_data["address"] = "bogus,192.168.1.23/32,fd12:3456:789a:1::1/128"
+    wg_config_data["allowed_ips"] = "0.0.0.0/0,::0/0,invalid"
     assert transform_wg_data(wg_config_data) == expected_result
 
 
